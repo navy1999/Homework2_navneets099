97c52a7295aa073e7abfe0dd9f74368ff1e4519c
Navneet Shankar <navneets099@gmail.com>
2024-09-29 17:59:38 -0400

    LBakery modified 2

--- src/main/java/edu/vt/ece/locks/TreePeterson.java
+++ src/main/java/edu/vt/ece/locks/TreePeterson.java
@@ -1,7 +1,9 @@
 package edu.vt.ece.locks;
-
+import edu.vt.ece.util.Tree;
 public class TreePeterson implements Lock {
-
+    private Peterson[] locks; // Each node of the binary tree is a peterson lock
+    private int n;
+    private int depth;
     public TreePeterson() {
         this(2);
     }
